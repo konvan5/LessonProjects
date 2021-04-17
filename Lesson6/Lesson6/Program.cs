6776938188ca46059efed7ae5b5882d647b732e3
konvan5 <konvan97@gmail.com>
2021-04-17 23:45:23 +0700

    Lesson 6

---
+++ Lesson6/Lesson6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Lesson6Library;
+using System.Collections.Generic;
+
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            User user = new User("Олег", 23);
+            user.PrintHello();
+            List<User> users = new List<User>();
+            users.Add(user);
+            users.Add(new User("Петя", 25));
+            users.Add(new User("Аня", 24));
+            users.Add(new User("Наташа", 26));
+            users.Add(new User("Вася", 30));
+            if (!DataBase.SaveUsers(users))
+                Console.WriteLine("Нам не удалось сохранить файл :(");
+            List<User> users2 = DataBase.LoadUsers();
+            if(users2 == null)
+                Console.WriteLine("Что-то пошло не так. У нас нет списка! :(");
+            foreach (var u in users2)
+            {
+                u.PrintHello();
+            }
+        }
+    }
+}
---
+++ Lesson6/Lesson6Library/Classes.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Lesson6Library
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public void PrintHello()
+        {
+            Console.WriteLine($"Hello! Im {Name}! Мне {Age} лет!");
+        }
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+    }
+
+    public static class DataBase
+    {
+        public static string userPath { get; set; } = "D:\\DataBaseUsers.txt";
+        public static bool SaveUsers(List<User> users)
+        {
+            StreamWriter fileIn;
+            try
+            {
+                fileIn = new StreamWriter(userPath);
+                foreach (var user in users)
+                {
+                    fileIn.WriteLine(user.Name);
+                    fileIn.WriteLine(user.Age);
+                }
+                fileIn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка! {ex.Message}");
+                return false;
+            }
+            return true;
+        }
+
+        public static List<User> LoadUsers()
+        {
+            List<User> users = new List<User>();
+            StreamReader fileOut;
+            try
+            {
+                if (File.Exists(userPath))
+                {
+                    fileOut = new StreamReader(userPath);
+                    while (!fileOut.EndOfStream)
+                    {
+                        string name = fileOut.ReadLine();
+                        int age = int.Parse(fileOut.ReadLine());
+                        users.Add(new User(name, age));
+                    }
+                    fileOut.Close();
+                }
+                else 
+                    throw new Exception("Файла не существует!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка! {ex.Message}");
+                return null;
+            }
+            return users;
+        }
+
+        public class Sklad
+        {
+            public Dictionary<Product, int> Products = new Dictionary<Product, int>(); // key - продукт, value - кол-во
+            public bool GiveMe(Product product, int count)
+            {
+                return true;
+                "12345-----".PadRight(10, '-');
+            }
+        }
+
+        public class Product
+        {
+            public string Name { get; set; }
+            public string Resume { get; set; }
+            public double Price { get; set; }
+        }
+    }
+}
